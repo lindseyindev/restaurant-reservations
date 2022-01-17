d7e9fd54d3913c0f6e21c614b37debc0fed109ef
LindseyinTech <lindseyintech@gmail.com>
2022-01-16 18:58:47 -0800

    updated backend added validation

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,9 +8,7 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed")
 
-router.route("/").get(controller.list).all(methodNotAllowed)
-
-router.route("/reservations/new")
+router.route("/").get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
